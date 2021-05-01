996db07293f8bb2941648489613c8bce58cdad9a
PAULO VICTOR TEODORIO DA SILVA <72501948+Teodorio@users.noreply.github.com>
2021-04-30 23:38:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let time = 6000;
+let currentImageIndex = 0
+let images = document.querySelectorAll("#slider img")
+let max = images.length;
+
+function nextImage() {
+    
+    
+    images[currentImageIndex].classList.remove("selected")
+    currentImageIndex++
+    if(currentImageIndex >= max -2)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
+}
+function prevImage() {
+
+    images[currentImageIndex].classList.remove("selected")
+    currentImageIndex--
+    if(currentImageIndex < 0)
+        currentImageIndex = max -3
+
+    images[currentImageIndex].classList.add("selected")
+    
+}
+function start() {
+    setInterval(() => {
+        nextImage();
+    }, time)
+}
+
+window.addEventListener("load", start)
